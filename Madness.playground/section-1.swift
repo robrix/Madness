1431349d1d99a10ee2b80e50822927eb9c79caee
Rob Rix <rob.rix@github.com>
2014-12-10 14:22:16 -0500

    Lambda terms are Printable.

--- Madness.playground/section-1.swift
+++ Madness.playground/section-1.swift
@@ -4,4 +4,15 @@ enum Term: Printable {
 	case Variable(String)
 	case Abstraction(String, Box<Term>)
 	case Application(Box<Term>, Box<Term>)
+
+	var description: String {
+		switch self {
+		case let Variable(symbol):
+			return symbol
+		case let Abstraction(symbol, body):
+			return "λ\(symbol).\(body.value.description)"
+		case let Application(x, y):
+			return "(\(x.value.description) \(y.value.description))"
+		}
+	}
 }
