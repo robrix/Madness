9f1650f7adcc9ef6c8ee2d16bfe1b9aa088b18f1
Rob Rix <rob.rix@github.com>
2014-12-09 21:38:58 -0500

    Test parsing literals.

--- MadnessTests/MadnessTests.swift
+++ MadnessTests/MadnessTests.swift
@@ -1,5 +1,10 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+import Madness
 import XCTest
 
-class MadnessTests: XCTestCase {}
+final class MadnessTests: XCTestCase {
+	func testLiteralParsersParseAPrefixOfTheInput() {
+		XCTAssertEqual(literal("foo")("foot")?.1 ?? "💀", "t")
+	}
+}
