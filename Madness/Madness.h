b42c3f7f41419c1992fe2c8a02bc8472b067e58a
Rob Rix <rob.rix@github.com>
2014-12-09 21:16:29 -0500

    Initial Commit

---
+++ Madness/Madness.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Madness.
+extern double MadnessVersionNumber;
+
+/// Project version string for Madness.
+extern const unsigned char MadnessVersionString[];
+
