4f2f3d973bbbfdaeed7eef0c9dd0bab6806ced2c
Rob Rix <rob.rix@github.com>
2014-12-10 14:22:48 -0500

    Parse the (appropriately parenthesized) lambda calculus.

--- Madness.playground/section-1.swift
+++ Madness.playground/section-1.swift
@@ -1,5 +1,7 @@
 import Box
 import Madness
+import Prelude
+
 enum Term: Printable {
 	case Variable(String)
 	case Abstraction(String, Box<Term>)
@@ -16,3 +18,19 @@ enum Term: Printable {
 		}
 	}
 }
+
+
+let symbol = range("a"..."z")
+
+let term: Parser<Term>.Function = fix { term in
+	let variable = map(symbol) { Term.Variable($0) }
+	let abstraction = map(literal("λ") ++ symbol ++ literal(".") ++ term) { Term.Abstraction($0.1.0, Box($0.1.1.1.0)) }
+	let application = map(literal("(") ++ term ++ literal(" ") ++ term ++ literal(")")) { Term.Application(Box($0.1.0), Box($0.1.1.1.0)) }
+	return (variable | abstraction | application)
+}
+
+let parse: String -> Term? = {
+	term($0)?.0
+}
+
+parse("λx.(x x)")?.description
