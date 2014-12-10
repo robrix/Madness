14b141ca7e4a5f496d6f39b2bceaecdbd38a6d1d
Rob Rix <rob.rix@github.com>
2014-12-09 23:46:07 -0500

    Assert Equality over Either.
    This had to be unpacked a bit, unfortunately.

--- MadnessTests/MadnessTests.swift
+++ MadnessTests/MadnessTests.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+import Either
 import Madness
 import XCTest
 
@@ -9,6 +10,18 @@ final class MadnessTests: XCTestCase {
 	func assertEqual<T: Equatable>(expression1: @autoclosure () -> T?, _ expression2: @autoclosure () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) {
 		let (actual, expected) = (expression1(), expression2())
 		if actual != expected { XCTFail("\(actual) is not equal to \(expected). " + message, file: file, line: line) }
+	}
+
+	func assertEqual<T: Equatable, U: Equatable>(expression1: @autoclosure () -> Either<T, U>?, _ expression2: @autoclosure () -> Either<T, U>?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) {
+		let (actual, expected) = (expression1(), expression2())
+		switch (actual, expected) {
+		case (.None, .None):
+			break
+		case let (.Some(x), .Some(y)) where x == y:
+			break
+		default:
+			XCTFail("\(actual) is not equal to \(expected). " + message, file: file, line: line)
+		}
 	}
 
 	func assertNil<T>(expression: @autoclosure () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) {
