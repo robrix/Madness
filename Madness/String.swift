78487123ee0e46bfbc899db980e8c5edbbf3a1b1
Rob Rix <rob.rix@github.com>
2014-12-10 01:45:05 -0500

    String.toOffset.

--- Madness/String.swift
+++ Madness/String.swift
@@ -4,4 +4,8 @@ extension String {
 	func fromOffset(offset: Int) -> String {
 		return self[advance(startIndex, offset, endIndex)..<endIndex]
 	}
+
+	func toOffset(offset: Int) -> String {
+		return self[startIndex..<advance(startIndex, offset, endIndex)]
+	}
 }
