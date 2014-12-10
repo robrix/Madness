a38b8f5c4790e695870a632758d9c82763fcfadb
Rob Rix <rob.rix@github.com>
2014-12-09 21:59:35 -0500

    Section the tests.

--- MadnessTests/MadnessTests.swift
+++ MadnessTests/MadnessTests.swift
@@ -4,6 +4,8 @@ import Madness
 import XCTest
 
 final class MadnessTests: XCTestCase {
+	// MARK: Assertions
+
 	func assertEqual<T: Equatable>(expression1: @autoclosure () -> T?, _ expression2: @autoclosure () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) {
 		let (actual, expected) = (expression1(), expression2())
 		if actual != expected { XCTFail("\(actual) is not equal to \(expected). " + message, file: file, line: line) }
@@ -18,6 +20,9 @@ final class MadnessTests: XCTestCase {
 		let actual = expression()
 		if actual == nil { XCTFail("\(actual) is nil. " + message, file: file, line: line) }
 	}
+
+
+	// MARK: Terminals
 
 	func testLiteralParsersParseAPrefixOfTheInput() {
 		assertEqual(literal("foo")("foot")?.1, "t")
