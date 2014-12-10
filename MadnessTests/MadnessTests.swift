9bf6c66136757c5495f636c92f504793d51c28c5
Rob Rix <rob.rix@github.com>
2014-12-09 21:45:56 -0500

    Easier testing with Optionals.

--- MadnessTests/MadnessTests.swift
+++ MadnessTests/MadnessTests.swift
@@ -4,7 +4,12 @@ import Madness
 import XCTest
 
 final class MadnessTests: XCTestCase {
+	func assertEqual<T: Equatable>(expression1: @autoclosure () -> T?, _ expression2: @autoclosure () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) {
+		let (actual, expected) = (expression1(), expression2())
+		if actual != expected { XCTFail("\(actual) is not equal to \(expected). " + message, file: file, line: line) }
+	}
+
 	func testLiteralParsersParseAPrefixOfTheInput() {
-		XCTAssertEqual(literal("foo")("foot")?.1 ?? "💀", "t")
+		assertEqual(literal("foo")("foot")?.1, "t")
 	}
 }
