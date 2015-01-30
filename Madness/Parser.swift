fca3bc6492566034cb54476092922e608995b0b9
Rob Rix <rob.rix@github.com>
2015-01-29 22:48:06 -0500

    Define a bind function.

--- Madness/Parser.swift
+++ Madness/Parser.swift
@@ -155,6 +155,18 @@ public func ignore(string: String) -> Parser<()>.Function {
 }
 
 
+// MARK: Binding
+
+/// Returns a parser which requires `parser` to parse, passes its parsed trees to a function `f`, and then requires the result of `f` to parse.
+///
+/// This can be used to conveniently make a parser which depends on earlier parsed input, for example to parse exactly the same number of characters, or to parse structurally significant indentation.
+public func >>= <T, U> (parser: Parser<T>.Function, f: T -> Parser<U>.Function) -> Parser<U>.Function {
+	return {
+		parser($0).map { f($0)($1) } ?? nil
+	}
+}
+
+
 // MARK: Private
 
 /// Defines concatenation for use in the `++` operator definitions above.
