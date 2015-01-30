b5877c71ee3b73de5739d92857efb8b6c1a41205
Rob Rix <rob.rix@github.com>
2015-01-29 22:49:19 -0500

    Define a Tree structure for the test.

--- MadnessTests/BindTests.swift
+++ MadnessTests/BindTests.swift
@@ -2,5 +2,30 @@
 
 import XCTest
 
+struct Tree<T: Equatable>: Equatable, Printable {
+	init(_ value: T, _ children: [Tree] = []) {
+		self.values = [ value ]
+		self.children = children
+	}
+
+	let values: [T]
+	let children: [Tree]
+
+
+	// MARK: Printable
+
+	var description: String {
+		let space = " "
+		let valueString = space.join(map(values, toString))
+		return children.count > 0 ?
+			"(\(valueString) \(space.join(map(children, toString))))"
+		:	"(\(valueString))"
+	}
+}
+
+func == <T: Equatable> (left: Tree<T>, right: Tree<T>) -> Bool {
+	return left.values == right.values && left.children == right.children
+}
+
 final class BindTests: XCTestCase {
 }
