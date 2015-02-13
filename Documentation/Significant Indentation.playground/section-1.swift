b5e2882e924b2377da83adcdb59592ba7fdab52a
Rob Rix <rob.rix@github.com>
2015-02-13 16:12:29 -0500

    Make the preamble disclosable.

--- Documentation/Significant Indentation.playground/section-1.swift
+++ Documentation/Significant Indentation.playground/section-1.swift
@@ -1,6 +1,3 @@
 import Box
 import Madness
 import Prelude
-
-let newline = %"\n"
-let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(%" ") ++ (any+ --> { "".join($0) }) ++ newline
---
+++ Documentation/Significant Indentation.playground/section-2.swift
@@ -0,0 +1,2 @@
+let newline = %"\n"
+let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(%" ") ++ (any+ --> { "".join($0) }) ++ newline
