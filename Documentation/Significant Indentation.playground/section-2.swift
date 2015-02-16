d2834e7744b64c6a85586c794fb54f10191e639e
Rob Rix <rob.rix@github.com>
2015-02-16 10:19:29 -0500

    Demonstrate header parsing.

--- Documentation/Significant Indentation.playground/section-2.swift
+++ Documentation/Significant Indentation.playground/section-2.swift
@@ -1,7 +1,13 @@
 // MARK: - Lexing rules
 
 let newline = ignore("\n")
-let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(%" ") ++ (any+ --> { "".join($0) }) ++ newline
 let ws = %" " | %"\t"
 let text = %("a"..."z") | %("A"..."Z") | %("0"..."9") | ws
 let restOfLine = (text+ --> { "".join($0) }) ++ newline
+
+
+// MARK: - Parsing rules
+
+let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(" ") ++ restOfLine
+
+let s = header("# Words\n")?.0
