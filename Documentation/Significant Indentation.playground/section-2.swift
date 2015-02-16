aa198e3314cec78f9b8573d41a77e3f8be79dfb9
Rob Rix <rob.rix@github.com>
2015-02-16 10:29:08 -0500

    Parse blockquotes.

--- Documentation/Significant Indentation.playground/section-2.swift
+++ Documentation/Significant Indentation.playground/section-2.swift
@@ -9,11 +9,14 @@ let restOfLine = (text+ --> { "".join($0) }) ++ newline
 // MARK: - AST
 
 enum Node: Printable {
+	case Blockquote([Node])
 	case Header(Int, String)
 
 
-	func analysis<T>(#ifHeader: (Int, String) -> T) -> T {
+	func analysis<T>(#ifBlockquote: [Node] -> T, ifHeader: (Int, String) -> T) -> T {
 		switch self {
+		case let Blockquote(nodes):
+			return ifBlockquote(nodes)
 		case let Header(level, text):
 			return ifHeader(level, text)
 		}
@@ -24,7 +27,8 @@ enum Node: Printable {
 
 	var description: String {
 		return analysis(
-			ifHeader: { "<h\($0)>\($1)</h\($0)>" })
+			ifBlockquote: { "<blockquote>\n\t" + "\n\t".join(lazy($0).map(toString)) + "\n</blockquote>" },
+		ifHeader: { "<h\($0)>\($1)</h\($0)>" })
 	}
 }
 
@@ -32,5 +36,6 @@ enum Node: Printable {
 // MARK: - Parsing rules
 
 let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(" ") ++ restOfLine --> { Node.Header($0, $1) }
+let blockquote = ignore(">") ++ ignore(" ") ++ restOfLine
 
 let s = header("# Words\n")?.0
