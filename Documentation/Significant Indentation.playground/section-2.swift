9f0ac64479528400361a7d531e7f0ed8fe8b9666
Rob Rix <rob.rix@github.com>
2015-02-16 10:19:41 -0500

    Construct AST nodes.

--- Documentation/Significant Indentation.playground/section-2.swift
+++ Documentation/Significant Indentation.playground/section-2.swift
@@ -6,8 +6,15 @@ let text = %("a"..."z") | %("A"..."Z") | %("0"..."9") | ws
 let restOfLine = (text+ --> { "".join($0) }) ++ newline
 
 
+// MARK: - AST
+
+enum Node {
+	case Header(Int, String)
+}
+
+
 // MARK: - Parsing rules
 
-let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(" ") ++ restOfLine
+let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(" ") ++ restOfLine --> { Node.Header($0, $1) }
 
 let s = header("# Words\n")?.0
