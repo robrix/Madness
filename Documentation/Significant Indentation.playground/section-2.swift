90f3b5703a4aa24b3a1b222350da80c49e754dd8
Rob Rix <rob.rix@github.com>
2015-02-16 10:18:37 -0500

    Text & rest of line.

--- Documentation/Significant Indentation.playground/section-2.swift
+++ Documentation/Significant Indentation.playground/section-2.swift
@@ -3,3 +3,5 @@
 let newline = ignore("\n")
 let header = ((%"#" * (1..<7)) --> { $0.count }) ++ ignore(%" ") ++ (any+ --> { "".join($0) }) ++ newline
 let ws = %" " | %"\t"
+let text = %("a"..."z") | %("A"..."Z") | %("0"..."9") | ws
+let restOfLine = (text+ --> { "".join($0) }) ++ newline
