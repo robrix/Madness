1552d6c0023af3f03cf37f2fdc42a5b4451e4412
Rob Rix <rob.rix@github.com>
2015-02-21 09:57:25 -0500

    Move concatenation into its own file.

--- Madness/Parser.swift
+++ Madness/Parser.swift
@@ -43,30 +43,6 @@ public prefix func %<I: IntervalType where I.Bound == Character>(interval: I) ->
 
 // MARK: - Nonterminals
 
-// MARK: Concatenation
-
-/// Parses the concatenation of `left` and `right`, pairing their parse trees.
-public func ++ <T, U> (left: Parser<T>.Function, right: Parser<U>.Function) -> Parser<(T, U)>.Function {
-	return concatenate(left, right)
-}
-
-/// Parses the concatenation of `left` and `right`, dropping `right`’s parse tree.
-public func ++ <T> (left: Parser<T>.Function, right: Parser<()>.Function) -> Parser<T>.Function {
-	return concatenate(left, right) --> { x, _ in x }
-}
-
-/// Parses the concatenation of `left` and `right`, dropping `left`’s parse tree.
-public func ++ <T> (left: Parser<()>.Function, right: Parser<T>.Function) -> Parser<T>.Function {
-	return concatenate(left, right) --> { $1 }
-}
-
-/// Parses the concatenation of `left` and `right, dropping both parse trees.
-public func ++ (left: Parser<()>.Function, right: Parser<()>.Function) -> Parser<()>.Function {
-	return ignore(concatenate(left, right))
-}
-
-
-
 // MARK: Alternation
 
 /// Parses either `left` or `right`.
@@ -185,18 +161,6 @@ public func >>- <T, U> (parser: Parser<T>.Function, f: T -> Parser<U>.Function) 
 
 
 // MARK: Private
-
-/// Defines concatenation for use in the `++` operator definitions above.
-private func concatenate<T, U>(left: Parser<T>.Function, right: Parser<U>.Function) -> Parser<(T, U)>.Function {
-	return {
-		left($0).map { x, rest in
-			right(rest).map { y, rest in
-				((x, y), rest)
-			}
-		} ?? nil
-	}
-}
-
 
 /// Defines alternation for use in the `|` operator definitions above.
 private func alternate<T, U>(left: Parser<T>.Function, right: Parser<U>.Function) -> Parser<Either<T, U>>.Function {
