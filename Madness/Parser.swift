66a15cd5abea191589c6e0a5640b4e3544beb531
Rob Rix <rob.rix@github.com>
2015-02-22 18:14:14 -0500

    Move --> into its own file.

--- Madness/Parser.swift
+++ Madness/Parser.swift
@@ -58,27 +58,6 @@ public prefix func %<I: IntervalType where I.Bound == Character>(interval: I) ->
 }
 
 
-// MARK: - Nonterminals
-
-// MARK: Mapping
-
-/// Returns a parser which maps parse trees into another type.
-public func --> <C: CollectionType, T, U>(parser: Parser<C, T>.Function, f: T -> U) -> Parser<C, U>.Function {
-	return {
-		parser($0).map { (f($0), $1) }
-	}
-}
-
-/// Returns a parser which maps parse trees into another type.
-///
-/// This overload also receives the index that parsing ended at.
-public func --> <C: CollectionType, T, U>(parser: Parser<C, T>.Function, f: (C, Range<C.Index>, T) -> U) -> Parser<C, U>.Function {
-	return { input, index in
-		parser(input, index).map { (f(input, index..<$1, $0), $1) }
-	}
-}
-
-
 // MARK: - Private
 
 /// Returns `true` iff `collection` contains all of the elements in `needle` in-order and contiguously, starting from `index`.
