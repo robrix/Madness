84be66088ed6ee4a371694c3045ae1209c48e47e
Rob Rix <rob.rix@github.com>
2015-03-20 18:07:46 -0400

    Define flatMap inline.

--- Madness/FlatMap.swift
+++ Madness/FlatMap.swift
@@ -4,9 +4,7 @@
 ///
 /// This can be used to conveniently make a parser which depends on earlier parsed input, for example to parse exactly the same number of characters, or to parse structurally significant indentation.
 public func >>- <C: CollectionType, T, U> (parser: Parser<C, T>.Function, f: T -> Parser<C, U>.Function) -> Parser<C, U>.Function {
-	return { input, index in
-		parser(input, index).flatMap { f($0)(input, $1) }
-	}
+	return { input, index in parser(input, index).flatMap { f($0)(input, $1) } }
 }
 
 
