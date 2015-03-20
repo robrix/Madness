61d8dcc512b15e5281d69337d771f4725ded692b
Rob Rix <rob.rix@github.com>
2015-03-20 13:19:41 -0400

    Case analysis for Error.

--- Madness/Error.swift
+++ Madness/Error.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 /// A composite error.
-public enum Error<I: ForwardIndexType> {
+public enum Error<I: ForwardIndexType>: Printable {
 	/// Constructs a leaf error, e.g. for terminal parsers.
 	public static func leaf(reason: String, _ index: I) -> Error {
 		return Leaf(reason, Box(index))
@@ -10,6 +10,17 @@ public enum Error<I: ForwardIndexType> {
 	/// Constructs a branch error, e.g. for nonterminal parsers.
 	public static func branch(errors: [Error]) -> Error {
 		return Branch(errors)
+	}
+
+
+	/// Case analysis.
+	public func analysis<Result>(@noescape #ifLeaf: (String, I) -> Result, @noescape ifBranch: [Error] -> Result) -> Result {
+		switch self {
+		case let Leaf(reason, index):
+			return ifLeaf(reason, index.value)
+		case let Branch(errors):
+			return ifBranch(errors)
+		}
 	}
 
 
