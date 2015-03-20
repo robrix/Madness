444ae8c0d4b278a2d5aeee68f9402732da6224ed
Rob Rix <rob.rix@github.com>
2015-03-20 17:48:43 -0400

    Ignored zero-or-more repetition uses the infix * operator.

--- Madness/Repetition.swift
+++ Madness/Repetition.swift
@@ -12,7 +12,7 @@ public postfix func * (string: String) -> Parser<String, [String]>.Function {
 
 /// Parses `parser` 0 or more times and drops its parse trees.
 public postfix func * <C: CollectionType> (parser: Parser<C, Ignore>.Function) -> Parser<C, Ignore>.Function {
-	return ignore(repeat(parser, 0..<Int.max))
+	return ignore(parser * (0..<Int.max))
 }
 
 /// Parses `parser` 1 or more times.
