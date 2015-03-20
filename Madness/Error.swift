b337d287652e33dc4169a2c2a52b712689f7cbb6
Rob Rix <rob.rix@github.com>
2015-03-20 12:51:57 -0400

    Static constructor functions for Error.

--- Madness/Error.swift
+++ Madness/Error.swift
@@ -2,6 +2,19 @@
 
 /// A composite error.
 public enum Error<I: ForwardIndexType> {
+	/// Constructs a leaf error, e.g. for terminal parsers.
+	public static func leaf(reason: String, _ index: I) -> Error {
+		return Leaf(reason, Box(index))
+	}
+
+	/// Constructs a branch error, e.g. for nonterminal parsers.
+	public static func branch(errors: [Error]) -> Error {
+		return Branch(errors)
+	}
+
+
+	// MARK: Cases
+
 	case Leaf(String, Box<I>)
 	case Branch([Error])
 }
