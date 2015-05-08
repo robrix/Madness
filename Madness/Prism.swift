ca7522472dc4fa5b52b22c8fec06384b1b1aa55b
Rob Rix <rob.rix@github.com>
2015-05-08 16:20:07 -0400

    Prism composition.

--- Madness/Prism.swift
+++ Madness/Prism.swift
@@ -4,4 +4,19 @@ public struct Prism<From, To> {
 	let forward: From -> To?
 	let backward: To -> From
 }
+
+
+// MARK: Composition
+
+public func >>> <From, Middle, To> (left: Prism<From, Middle>, right: Prism<Middle, To>) -> Prism<From, To> {
+	return compose(right, left)
+}
+
+private func compose<A, B, C>(g: Prism<B, C>, f: Prism<A, B>) -> Prism<A, C> {
+	return Prism(
+		forward: { f.forward($0).flatMap(g.forward) },
+		backward: f.backward <<< g.backward)
+}
+
+
 import Prelude
