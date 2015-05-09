156c625e63d78c3e5239030b357e72ba4affb0d0
Rob Rix <rob.rix@github.com>
2015-05-08 20:16:30 -0400

    Alternate Prism-parser-printers.

--- Madness/Literals.swift
+++ Madness/Literals.swift
@@ -13,6 +13,13 @@ func concatenate<T, U, V>(left: Prism<Stream<T>, U>, right: Prism<Stream<T>, V>)
 		backward: { left.backward($0.0) ++ right.backward($0.1) })
 }
 
+func alternate<T, U, V>(left: Prism<Stream<T>, U>, right: Prism<Stream<T>, V>) -> Prism<Stream<T>, Either<U, V>> {
+	return Prism(
+		forward: { left.forward($0).map(Either.left) ?? right.forward($0).map(Either.right) },
+		backward: { $0.either(ifLeft: left.backward, ifRight: right.backward) })
+}
 
+
+import Either
 import Prelude
 import Stream
