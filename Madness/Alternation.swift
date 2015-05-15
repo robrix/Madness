19bbe18ef1fde152d4ba3a835405c36034aa5d9d
Rob Rix <rob.rix@github.com>
2015-05-15 19:10:27 -0400

    Remove parse-tree-dropping postfix |?.

--- Madness/Alternation.swift
+++ Madness/Alternation.swift
@@ -5,11 +5,6 @@ public postfix func |? <C: CollectionType, T> (parser: Parser<C, T>.Function) ->
     return first <^> parser * (0...1)
 }
 
-/// Parses `parser` 0 or one time dropping the parse tree.
-public postfix func |? <C: CollectionType, T> (parser: Parser<C, Ignore>.Function) -> Parser<C, Ignore>.Function {
-    return ignore(parser * (0...1))
-}
-
 
 /// Parses either `left` or `right`.
 public func <||> <C: CollectionType, T, U> (left: Parser<C, T>.Function, right: Parser<C, U>.Function) -> Parser<C, Either<T, U>>.Function {
