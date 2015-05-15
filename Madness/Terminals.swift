ae5492aaf2a53154fdbbf6bdd72966c7d9cb9b34
Rob Rix <rob.rix@github.com>
2015-05-15 15:17:39 -0400

    Move nonterminals into their own file.

--- Madness/Terminals.swift
+++ Madness/Terminals.swift
@@ -7,19 +7,4 @@ func literal<T: Equatable>(element: T) -> Prism<Stream<T>, T> {
 }
 
 
-func concatenate<T, U, V>(left: Prism<Stream<T>, U>, right: Prism<Stream<T>, V>) -> Prism<Stream<T>, (U, V)> {
-	return Prism(
-		forward: { (left.forward($0) &&& right.forward($0.rest)) },
-		backward: { left.backward($0.0) ++ right.backward($0.1) })
-}
-
-func alternate<T, U, V>(left: Prism<Stream<T>, U>, right: Prism<Stream<T>, V>) -> Prism<Stream<T>, Either<U, V>> {
-	return Prism(
-		forward: { left.forward($0).map(Either.left) ?? right.forward($0).map(Either.right) },
-		backward: { $0.either(ifLeft: left.backward, ifRight: right.backward) })
-}
-
-
-import Either
-import Prelude
 import Stream
