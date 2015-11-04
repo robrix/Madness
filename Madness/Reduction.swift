0bd8b02dcf4a01036fc805588ab609f07bba3117
joshvera <vera@github.com>
2015-11-04 15:10:35 -0500

    Add an override for -->

--- Madness/Reduction.swift
+++ Madness/Reduction.swift
@@ -1,11 +1,18 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 /// Returns a parser which maps parse trees into another type.
-public func --> <C: CollectionType, T, U>(parser: Parser<C, T>.Function, f: (C, Range<C.Index>, T) -> U) -> Parser<C, U>.Function {
-	return { input, sourcePos in
-		parser(input, sourcePos).map { (f(input, sourcePos.index..<$1.index, $0), $1) }
+public func --> <C: CollectionType, T, U>(parser: Parser<C, T>.Function, f: (C, SourcePos<C.Index>, Range<C.Index>, T) -> U) -> Parser<C, U>.Function {
+	return { input, inputPos in
+		parser(input, inputPos).map { output, outputPos in
+			(f(input, outputPos, inputPos.index..<outputPos.index, output), outputPos)
+		}
 	}
 }
+
+public func --> <C: CollectionType, T, U>(parser: Parser<C, T>.Function, f: (SourcePos<C.Index>, Range<C.Index>, T) -> U) -> Parser<C, U>.Function {
+	return parser --> { f($1, $2, $3) }
+}
+
 
 
 /// Returns a parser which maps parse results.
