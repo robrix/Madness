6c902fb2d14b5e35e45eb64afbadd7986ca4efec
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-02 22:14:58 +0900

    Tests for between

--- MadnessTests/CombinatorTests.swift
+++ MadnessTests/CombinatorTests.swift
@@ -1,35 +1,24 @@
-//
-//  CombinatorTests.swift
-//  Madness
-//
-//  Created by Yoshiki Kudo on 2015/12/02.
-//  Copyright © 2015年 Rob Rix. All rights reserved.
-//
+//  Copyright © 2015 Rob Rix. All rights reserved.
 
-import XCTest
-
-class CombinatorTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+final class CombinatorTests: XCTestCase {
+	
+	// MARK: - between
+	
+	let braces: StringParser -> StringParser = between(%"{", %"}")
+	
+	func testBetweenCombinatorParsesSandwichedString(){
+		assertTree(braces(%"a"), "{a}".characters, ==, "a")
+	}
+	
+	func testBetweenCombinatorAcceptsEmptyString(){
+		assertTree(braces(%""), "{}".characters, ==, "")
+	}
+	
 }
+
+
+// MARK: - Imports
+
+import Assertions
+import Madness
+import XCTest
