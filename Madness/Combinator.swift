78874ba80f5158616572430eb1835ab43bcdc837
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-02 22:43:19 +0900

    Implement manyTill

--- Madness/Combinator.swift
+++ Madness/Combinator.swift
@@ -4,3 +4,8 @@
 public func between<C: CollectionType, T, U, V>(open: Parser<C, T>.Function, _ close: Parser<C, U>.Function)(_ parser: Parser<C, V>.Function) -> Parser<C, V>.Function {
 	return open *> parser <* close
 }
+
+///
+public func manyTill<C: CollectionType, T, U>(parser: Parser<C, T>.Function)(_ end: Parser<C, U>.Function) -> Parser<C, [T]>.Function {
+	return many(parser) <* end
+}
