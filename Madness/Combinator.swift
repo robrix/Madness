266bcc1e1f76c8689ca486ad80bcd1c31a509395
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2015-12-04 20:27:21 +0900

    Modify args

--- Madness/Combinator.swift
+++ Madness/Combinator.swift
@@ -1,8 +1,8 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 /// Parses `open`, followed by `parser` and `close`. Returns the value returned by `parser`.
-public func between<C: CollectionType, T, U, V>(open: Parser<C, T>.Function, _ close: Parser<C, U>.Function)(_ parser: Parser<C, V>.Function) -> Parser<C, V>.Function {
-	return open *> parser <* close
+public func between<C: CollectionType, T, U, V>(open: Parser<C, T>.Function, _ close: Parser<C, U>.Function) -> Parser<C, V>.Function -> Parser<C, V>.Function {
+	return { open *> $0 <* close }
 }
 
 ///
